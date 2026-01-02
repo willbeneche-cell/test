118e43db570493fed72a7005d23795abf6751a05
willbeneche-cell <willbeneche@gmail.com>
2026-01-02 17:53:41 +0100

    Delete cursor.js

--- cursor.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-    const aura = document.getElementById("cursor-aura");
-    aura.style.top = `${e.clientY}px`;
-    aura.style.left = `${e.clientX}px`;
-});
